8301b94f18058619ed472a17c8f757fd83d62616
Gusmayadi <umaylab@gmail.com>
2022-12-17 14:58:41 +0700

    Initial Commit

---
+++ Firebase-DemoUmay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase-DemoUmay
+//
+//  Created by MacUmay on 17/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
